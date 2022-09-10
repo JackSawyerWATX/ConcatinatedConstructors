5fc3f716a00614d3d3d616905424c08d6fe8e631
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-09-09 20:33:22 -0700

    added another constructor

--- Main.java
+++ Main.java
@@ -2,18 +2,19 @@
 public class Main {
     String x;
     String y;
-
+    String z;
   
     // Create a class constructor for the Main class
     public Main() {
       x = "Danny Boy O'Connor";
       y = "Everlast";
+      z = "Lethal";
     }
   
     public static void main(String[] args) {
       Main boxer1 = new Main();
       
-      System.out.println(boxer1.y + ", & " + boxer1.x);
+      System.out.println(boxer1.y + ", & " + boxer1.z + ", and sometimes " + boxer1.x);
       
     }
   }
