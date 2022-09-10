43643f4f7644820bf3dafdc96bdd49a687f1a218
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-09-09 20:29:55 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+// Create a Main class
+public class Main {
+    String x;
+  
+    // Create a class constructor for the Main class
+    public Main() {
+      x = "Danny Boy O'Connor";
+    }
+  
+    public static void main(String[] args) {
+      Main boxer1 = new Main();
+      
+      System.out.println(boxer1.x);
+      
+    }
+  }
+  
